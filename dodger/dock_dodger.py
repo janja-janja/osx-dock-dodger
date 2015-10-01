15f7fb4ec11989cf2674dae5960cbac1e1714500
Master Yoda <deebeat@students.uonbi.ac.ke>
2015-10-01 13:51:55 +0300

    Replace single quotes with double in BaseColors class

--- dodger/dock_dodger.py
+++ dodger/dock_dodger.py
@@ -7,14 +7,14 @@ class BaseColors(object):
     """
     Holds color codes to be used in the `terminal`
     """
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDCOLOR = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    HEADER = "\033[95m"
+    BLUE = "\033[94m"
+    GREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDCOLOR = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 class OSXDodger(BaseColors):
